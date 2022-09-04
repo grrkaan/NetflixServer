70455c8fd55f04b85b2c87e5ef120572d106c53a
Kaan Sercan Görür <kaangorur@hotmail.com>
2022-09-04 16:58:10 +0300

    Initial commit

---
+++ src/test/java/com/movie/NetflixServer/NetflixServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.NetflixServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
